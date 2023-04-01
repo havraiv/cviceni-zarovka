896f0921be07b119038944b06c762ae77d9d8fd5
Iva Havranová <havranova.i@seznam.cz>
2023-04-01 13:37:27 +0200

    Hotovo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('funguju!');
+// Žárovka
+// Vytvořte si repozitář ze šablony cviceni-zarovka se stránkou, která zobrazuje obyčejnou žárovku.
+
+// Pomocí JavaScriptu zařiďte, aby se při stisknutí libovolné klávesy na stránce přidala k elementu žárovky CSS třída bulb--on. Žárovka by se takto měla rozsvítit.
+// Vylepšete program tak, aby na následné stisknutí libovolné klávesy žárovka opět zhasnula. Opakovaným mačkáním kláves ji tak můžeme rozsvěcovat a zhasínat.
+
+const lightUp = () => {
+  const bulbElm = document.querySelector('.bulb');
+  bulbElm.classList.toggle('bulb--on');
+};
+
+document.addEventListener('keyup', lightUp);
